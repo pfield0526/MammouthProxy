8b909cca7c04f67bce593fe7609e99c6efc220b2
pfield0526 <pfield0526@gmail.com>
2025-07-07 11:20:19 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+```javascript
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Hardhat Express Web Service!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
+});
+```
